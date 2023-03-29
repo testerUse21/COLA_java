1e09c6fb7929b915c405f1608e9da62851da5943
testerUse21 <106081205+testerUse21@users.noreply.github.com>
2023-03-29 18:48:55 +0530

    Update MavenWrapperDownloader.java

--- .mvn/wrapper/MavenWrapperDownloader.java
+++ .mvn/wrapper/MavenWrapperDownloader.java
@@ -47,6 +47,10 @@ public class MavenWrapperDownloader {
 
     public static void main(String args[]) {
         System.out.println("- Downloader started");
+        System.out.println("New isuue");
+        System.out.println("New isuue");
+        System.out.println("New isuue");
+        System.out.println("New isuue");
         File baseDirectory = new File(args[0]);
         System.out.println("- Using base directory: " + baseDirectory.getAbsolutePath());
 
@@ -86,7 +90,7 @@ public class MavenWrapperDownloader {
         try {
             downloadFileFromURL(url, outputFile);
             System.out.println("Done");
-            System.exit(0);
+           // System.exit(0);
         } catch (Throwable e) {
             System.out.println("- Error downloading");
             e.printStackTrace();
